3ec0d8c72757b76cf776a2008c1267d88368a1fa
aliskhangazamatov <ag@farcana.com>
2024-06-10 22:43:40 +0300

    add middleware, appError class

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,11 +4,13 @@ const uuid = require('uuid')
 const mailService = require('./mail-service')
 const tokenService = require('../service/token-service')
 const UserDto = require('../dtos/user-dto')
+const AppError = require('../exeptions/api-error')
+
 class UserService{
   async registration(email, password) {
     const candidate = await UserModel.findOne({email})
     if (candidate) {
-      throw new Error(`Пользователь с таким ${email} уже существует`)
+      throw AppError.BadRequest(`Пользователь с таким ${email} уже существует`)
     }
     const hashPassword = await bcrypt.hash(password, 3);
     const activationLink = uuid.v4(); // v34fa-asfasf-142saf-sa-asf
@@ -21,6 +23,15 @@ class UserService{
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
     return {...tokens, user: userDto}
+  }
+  async activate(activationLink){
+    const user = await UserModel.findOne({activationLink});
+
+    if(!user){
+      throw AppError.BadRequest('Некорректная ссылка');
+    }
+    user.isActivated = true;
+    await user.save();
   }
 }
 
