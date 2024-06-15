a3dcb3767904ace683975056053389eef48e2f9b
aliskhangazamatov <ag@farcana.com>
2024-06-15 19:11:03 +0300

    finally backend

--- server/middlewares/error-middleware.js
+++ server/middlewares/error-middleware.js
@@ -1,9 +1,10 @@
-const ApiError = require('../exeptions/api-error')
+const ApiError = require('../exeptions/api-error');
 
-module.exports = function(err, req, res, next){
-  console.log(res);
-  if(err instanceof ApiError){
+module.exports = function (err, req, res, next) {
+  console.log(err);
+  if (err instanceof ApiError) {
     return res.status(err.status).json({message: err.message, errors: err.errors})
   }
-  return res.status(500).json({message: 'Что-то пошло не так ('})
-}
+  return res.status(500).json({message: 'Непредвиденная ошибка'})
+
+};
